e9bbc4cf66d3524f9631abd2d23441a58fa1505a
mbunge <bunge@jungehaie.com>
2021-07-25 23:50:00 +0200

    feat(app) Remove unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProComponent } from './pages/pro/pro.component';
 
 @NgModule({
   declarations: [

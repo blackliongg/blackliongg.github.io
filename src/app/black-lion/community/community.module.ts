4ec3f621917ec857cd77f7288ba6973de723a4af
mbunge <bunge@jungehaie.com>
2021-07-27 23:24:33 +0200

    feat(app) Initialize community domain and community page

---
+++ src/app/black-lion/community/community.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommunityPageComponent } from './community-page/community-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    CommunityPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CommunityModule { }

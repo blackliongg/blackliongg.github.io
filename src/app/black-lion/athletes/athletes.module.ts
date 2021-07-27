dd88f5052645a44ea1415bcb165a46a1d27bd29d
mbunge <bunge@jungehaie.com>
2021-07-27 12:26:41 +0200

    fix(app) Use hero and section to layout progaming page

--- src/app/black-lion/athletes/athletes.module.ts
+++ src/app/black-lion/athletes/athletes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProgamingPageComponent } from './progaming-page/progaming-page.component';
 import {AthletesRoutingModule} from './athletes-routing.module';
+import {SharedModule} from '../../shared/shared.module';
 
 
 
@@ -11,7 +12,8 @@ import {AthletesRoutingModule} from './athletes-routing.module';
   ],
   imports: [
     CommonModule,
-    AthletesRoutingModule
+    AthletesRoutingModule,
+    SharedModule
   ]
 })
 export class AthletesModule { }

03337a711d3a71b1263de0a5da586771548f96be
mbunge <bunge@jungehaie.com>
2021-09-17 12:38:02 +0200

    feat: Implementiere Cookieconsent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgcCookieConsentConfig, NgcCookieConsentModule} from 'ngx-cookieconsent';
+import {environment} from '../environments/environment';
+
+const cookieConfig: NgcCookieConsentConfig = {
+  cookie: {
+    domain: environment.cookieConsent.cookieDomain // or 'your.domain.com' // it is mandatory to set a domain, for cookies to work properly (see https://goo.gl/S2Hy2A)
+  },
+  palette: {
+    popup: {
+      background: '#000'
+    },
+    button: {
+      background: '#f69'
+    }
+  },
+  theme: 'edgeless',
+  type: 'opt-out',
+  content: {
+    header: "Cookies werden auf dieser Webseite genutzt!",
+    message: "Die Webseite nutzt Cookies um eine hervoragende Nutzererfahrung sicherzustellen.",
+    dismiss: 'Verstanden!',
+    allow: 'Cookies zulassen',
+    deny: 'Ablehnen',
+    link: 'Mehr erfahren',
+    policy: 'Cookie Richtlinie'
+  }
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +37,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgcCookieConsentModule.forRoot(cookieConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

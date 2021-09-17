64ee0632e86b5dadb81c757fd106bf4dffaa2d7a
mbunge <bunge@jungehaie.com>
2021-09-17 12:55:00 +0200

    feat: Passe Text zur Cookierichtlinie an.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,13 +21,13 @@ const cookieConfig: NgcCookieConsentConfig = {
   theme: 'edgeless',
   type: 'opt-out',
   content: {
-    header: "Cookies werden auf dieser Webseite genutzt!",
-    message: "Die Webseite nutzt Cookies um eine hervoragende Nutzererfahrung sicherzustellen.",
+    header: 'Cookies werden auf dieser Webseite genutzt!',
+    message: 'Wir verwenden Cookies um Inhalte und Anzeigen zu personalisieren, um Social-Media-Funktionen zur Verfügung zu stellen und unseren Traffic zu analysieren. Wir teilen auch Informationen über Ihre Nutzung unserer Website mit unseren Social Media-, Werbe- und Analysepartnern.',
     dismiss: 'Verstanden!',
     allow: 'Cookies zulassen',
     deny: 'Ablehnen',
-    link: 'Mehr erfahren',
-    policy: 'Cookie Richtlinie'
+    link: 'Mehr Details',
+    policy: 'Cookierichtlinie anzeigen'
   }
 };
 

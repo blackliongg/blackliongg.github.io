f26891134ebca5bce598c6c5e7ddfa5390c116ed
mbunge <bunge@jungehaie.com>
2021-09-30 17:32:30 +0200

    feat: Ermögliche Sponsorenkontakt.

--- src/app/black-lion/about/about-page/about-page.component.ts
+++ src/app/black-lion/about/about-page/about-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subject, Subscription} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.scss']
 })
-export class AboutPageComponent implements OnInit {
+export class AboutPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private debounce: Subject<boolean> = new Subject<boolean>();
+  private debounceSubscription: Subscription | null = null;
+
+  constructor() {
+    this.debounce.pipe(
+      debounceTime(400),
+    );
+  }
 
   ngOnInit(): void {
+    this.debounce.asObservable().subscribe(() => AboutPageComponent.mailTo());
+  }
+
+  ngOnDestroy(): void {
+    if(this.debounceSubscription) {
+      this.debounceSubscription.unsubscribe();
+    }
+  }
+
+  private static mailTo(): void {
+    window.open('mailto:info@darkorigin.de?cc=benjamin.schlotzhauer@darkorigin.de&subject=BLACK LION SPONSORING&body=');
+  }
+
+  triggerMailTo() {
+    this.debounce.next(true);
   }
 
 }
